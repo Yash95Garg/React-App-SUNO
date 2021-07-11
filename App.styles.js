0bab0637cb8f37750a7591ca9dd813f5819cc204
Yash95Garg <19uec132@lnmiit.ac.in>
2021-07-11 22:56:30 +0530

    yy

---
+++ App.styles.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const App = styled.div`
+  background-image: linear-gradient(to right,#FFEEEE,#DDEFBB);
+  height: 100vh;
+  display: flex
+`
+
+export const Header = styled.h1`
+  color: pink
+`
